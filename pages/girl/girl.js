e8375fa98340a4a4f60c73e6be5857ccda09615d
wangfeng19930909 <1300497680@qq.com>
2019-01-09 17:46:52 +0800

    视频模块基本框架完成

--- pages/girl/girl.js
+++ pages/girl/girl.js
@@ -18,128 +18,6 @@ Page({
     col2: [],
     // loadingCount: 0,
     // images: []
-    // loadingCount: 20,
-    // images: [
-    //   {
-    //     id: "5c2dabdb9d21226e068debf9",
-    //     url: "https://ws1.sinaimg.cn/large/0065oQSqly1fytdr77urlj30sg10najf.jpg",
-    //     height: 0
-    //   },
-
-    //   {
-    //     id: "5c25db189d21221e8ada8664",
-    //     url: "https://ws1.sinaimg.cn/large/0065oQSqly1fymj13tnjmj30r60zf79k.jpg",
-    //     height: 0
-    //   },
-
-    //   {
-    //     id: "5c12216d9d21223f5a2baea2",
-    //     url: "https://ws1.sinaimg.cn/large/0065oQSqgy1fy58bi1wlgj30sg10hguu.jpg",
-    //     height: 0
-    //   },
-
-    //   {
-    //     id: "5bfe1a5b9d2122309624cbb7",
-    //     url: "https://ws1.sinaimg.cn/large/0065oQSqgy1fxno2dvxusj30sf10nqcm.jpg",
-    //     height: 0
-    //   },
-
-    //   {
-    //     id: "5bf22fd69d21223ddba8ca25",
-    //     url: "https://ws1.sinaimg.cn/large/0065oQSqgy1fxd7vcz86nj30qo0ybqc1.jpg",
-    //     height: 0
-    //   },
-
-    //   {
-    //     id: "5be14edb9d21223dd50660f8",
-    //     url: "https://ws1.sinaimg.cn/large/0065oQSqgy1fwyf0wr8hhj30ie0nhq6p.jpg",
-    //     height: 0
-    //   },
-
-    //   {
-    //     id: "5bcd71979d21220315c663fc",
-    //     url: "https://ws1.sinaimg.cn/large/0065oQSqgy1fwgzx8n1syj30sg15h7ew.jpg",
-    //     height: 0
-    //   },
-
-    //   {
-    //     id: "5bc434ac9d212279160c4c9e",
-    //     url: "https://ws1.sinaimg.cn/large/0065oQSqly1fw8wzdua6rj30sg0yc7gp.jpg",
-    //     height: 0
-    //   },
-
-    //   {
-    //     id: "5bbb0de09d21226111b86f1c",
-    //     url: "https://ws1.sinaimg.cn/large/0065oQSqly1fw0vdlg6xcj30j60mzdk7.jpg",
-    //     height: 0
-    //   },
-
-    //   {
-    //     id: "5ba206ec9d2122610aba3440",
-    //     url: "https://ws1.sinaimg.cn/large/0065oQSqly1fvexaq313uj30qo0wldr4.jpg",
-    //     height: 0
-    //   },
-
-    //   {
-    //     id: "5b9771a29d212206c1b383d0",
-    //     url: "https://ws1.sinaimg.cn/large/0065oQSqly1fv5n6daacqj30sg10f1dw.jpg",
-    //     height: 0
-    //   },
-
-    //   {
-    //     id: "5b830bba9d2122031f86ee51",
-    //     url: "https://ws1.sinaimg.cn/large/0065oQSqly1fuo54a6p0uj30sg0zdqnf.jpg",
-    //     height: 0
-    //   },
-
-    //   {
-    //     id: "5b7b836c9d212201e982de6e",
-    //     url: "https://ws1.sinaimg.cn/large/0065oQSqly1fuh5fsvlqcj30sg10onjk.jpg",
-    //     height: 0
-    //   },
-
-    //   {
-    //     id: "5b74e9409d21222c52ae4cb4",
-    //     url: "https://ws1.sinaimg.cn/large/0065oQSqly1fubd0blrbuj30ia0qp0yi.jpg",
-    //     height: 0
-    //   },
-
-    //   {
-    //     id: "5b7102749d2122341d563844",
-    //     url: "https://ww1.sinaimg.cn/large/0065oQSqly1fu7xueh1gbj30hs0uwtgb.jpg",
-    //     height: 0
-    //   },
-
-    //   {
-    //     id: "5b6bad449d21226f45755582",
-    //     url: "https://ww1.sinaimg.cn/large/0065oQSqgy1fu39hosiwoj30j60qyq96.jpg",
-    //     height: 0
-    //   },
-
-    //   {
-    //     id: "5b67b7fd9d2122195bdbd806",
-    //     url: "https://ww1.sinaimg.cn/large/0065oQSqly1ftzsj15hgvj30sg15hkbw.jpg",
-    //     height: 0
-    //   },
-
-    //   {
-    //     id: "5b63cd4e9d21225e0d3f58c9",
-    //     url: "https://ww1.sinaimg.cn/large/0065oQSqgy1ftwcw4f4a5j30sg10j1g9.jpg",
-    //     height: 0
-    //   },
-
-    //   {
-    //     id: "5b6151509d21225206860f08",
-    //     url: "https://ww1.sinaimg.cn/large/0065oQSqly1ftu6gl83ewj30k80tites.jpg",
-    //     height: 0
-    //   },
-
-    //   {
-    //     id: "5b60356a9d212247776a2e0e",
-    //     url: "http://ww1.sinaimg.cn/large/0065oQSqgy1ftt7g8ntdyj30j60op7dq.jpg",
-    //     height: 0
-    //   }
-    // ]
   },
 
   /**
@@ -173,8 +51,6 @@ Page({
     let scale = imgWidth / oImgW; //比例计算
     let imgHeight = oImgH * scale; //自适应高度
 
-    // let images = this.data.images;
-    // let images = this.data.girlData.images;
     let images = this.all_images;
 
     console.log("hhhhhhhhhhhhhhhhhhh");
@@ -192,8 +68,6 @@ Page({
 
     imageObj.height = imgHeight;
 
-    // let loadingCount = this.data.loadingCount - 1;
-    // let loadingCount = this.data.girlData.loadingCount - 1;
     let loadingCount = this.all_loadingCount - 1;
    
     let col1 = this.data.col1;
--- pages/video/video.js
+++ pages/video/video.js
@@ -1,66 +1,106 @@
-// pages/main/video/video.js
+var app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    hotVideos: {},//热点
+    jokeVideos: {},//搞笑
+    yuleVideos: {},//娱乐
+    goodVideos: {},//精品
+    //上方tab
+    navTab: ["热点", "搞笑", "娱乐","精品"],
+    currentNavtab: "0",
+    indicatorDots: false
+  },
 
+  switchTab: function (e) {
+    this.setData({
+      currentNavtab: e.currentTarget.dataset.idx
+    });
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    var tag1 = "V9LG4B3A0";
+    var tag2 = "V9LG4E6VR";
+    var tag3 = "V9LG4CHOR";
+    var tag4 = "00850FRB";
+    // http://c.3g.163.com/nc/video/list/VAP4BGTVD/n/1-10.html
+    
+    var page = 1;
 
+    var hotVideosUrl = app.globalData.videoBase +
+      "/nc/video/list/" + tag1 + "/n/" + page + "-10.html";
+    var jokeVideosUrl = app.globalData.videoBase +
+      "/nc/video/list/" + tag2 + "/n/" + page + "-10.html";
+    var yuleVideosUrl = app.globalData.videoBase +
+      "/nc/video/list/" + tag3 + "/n/" + page + "-10.html";
+    var goodVideosUrl = app.globalData.videoBase +
+      "/nc/video/list/" + tag4 + "/n/" + page + "-10.html";
+
+    this.getVideoListData(hotVideosUrl, "hotVideos", "热点");
+    this.getVideoListData(jokeVideosUrl, "jokeVideos", "搞笑");
+    this.getVideoListData(yuleVideosUrl, "yuleVideos", "娱乐");
+    this.getVideoListData(goodVideosUrl, "goodVideos", "精品");
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  getVideoListData: function (url, settedKey, categoryTitle) {
+    var that = this;
+    console.log(url)
+    wx.request({
+      url: url,
+      method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+      header: {
+        "Content-Type": "json"
+      },
+      success: function (res) {
+        console.log(res)
+        // that.processDoubanData(res.data, settedKey, categoryTitle)
+      },
+      fail: function (error) {
+        // fail
+        console.log(error)
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  processDoubanData: function (booksDouban, settedKey, categoryTitle) {
+    var books = [];
+    for (var idx in booksDouban.books) {
+      var subject = booksDouban.books[idx];
+      var title = subject.title;
+      if (title.length >= 10) {
+        title = title.substring(0, 10) + "...";
+      }
+      var author = subject.author;
+      var publisher = subject.publisher;
+      var pubdate = subject.pubdate;
+      var summary = subject.summary;
+      if (summary.length >= 30) {
+        summary = "&nbsp;&nbsp;" + summary.substring(0, 30) + "...";
+      }
+      // [1,1,1,1,1] [1,1,1,0,0]
+      var temp = {
+        stars: util.convertToStarsArray(subject.rating.average / 2),
+        title: title,
+        average: subject.rating.average,
+        coverageUrl: subject.images.large,
+        bookId: subject.id,
+        publisher: author + "/" + publisher + "/" + pubdate,
+        summary: summary
+      }
+      books.push(temp)
+    }
+    var readyData = {};
+    readyData[settedKey] = {
+      categoryTitle: categoryTitle,
+      books: books
+    }
+    this.setData(readyData);
+    console.log(readyData);
   }
+
 })
